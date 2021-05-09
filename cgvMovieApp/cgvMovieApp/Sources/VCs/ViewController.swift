a0001f0a59245a85273879c51e57c0a4b53305e7
pcsoyeon <alice9809@naver.com>
2021-05-09 21:43:36 +0900

    Initial commit

---
+++ cgvMovieApp/cgvMovieApp/Sources/VCs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cgvMovieApp
+//
+//  Created by soyeon on 2021/05/09.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
