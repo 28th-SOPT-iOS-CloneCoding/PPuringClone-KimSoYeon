7158a2558b70c5b2098e1f1bb2aa6927481deeb9
pcsoyeon <alice9809@naver.com>
2021-06-21 00:34:52 +0900

    Initial Project Setting

---
+++ ReminderApp/ReminderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReminderApp
+//
+//  Created by soyeon on 2021/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
